00cc7bc1b0fe329585a685db4abe662fa684cee8
Raj Khare <raj.khare.java@gmail.com>
2024-10-05 20:33:11 +0530

    5-Oct-2024:update

---
+++ code/section7/src/RelationalOperatorDemo.java
@@ -0,0 +1,18 @@
+public class RelationalOperatorDemo {
+
+    public static void main(String[] args) {
+
+        int num1 = 9;
+        int num2 = 9;
+        int num3 = 9;
+        boolean isSame = num1 == num2;
+
+        boolean a = true;
+        boolean b = false;
+        boolean isEqual = a == b;
+
+        boolean isSimilar = (num1 == num2)  && (num2== num3);
+
+    }
+
+}
