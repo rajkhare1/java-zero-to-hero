95c3254e7bc5d6d9d84361f9c5dc92059722d8e0
Raj Khare <raj.khare.java@gmail.com>
2024-10-07 08:24:48 +0530

    7-Oct-2024:update

--- code/section7/src/RelationalOperatorDemo.java
+++ code/section7/src/RelationalOperatorDemo.java
@@ -13,6 +13,28 @@ public class RelationalOperatorDemo {
 
         boolean isSimilar = (num1 == num2)  && (num2== num3);
 
-    }
+        int num4 = 5;
+        int num5 = 10;
+        boolean isNotEqual = num4 != num5;
+
+        boolean bool1 = true;
+        boolean bool2 = false;
+        isNotEqual = bool1 != bool2;
+
+        double num6 = 3.14;
+        double num7 = 3.14;
+        isNotEqual = num6 != num7;
+
+        boolean isSimilar1 = (num1 != num2) && ( num2 != num3);
+
+        int num8 = 9;
+        int num9 = 6;
+        double num10 = Double.NaN;
+        boolean isGreaterThanOrEqal;
+        isGreaterThanOrEqal = num8 >= num9;// true
+        isGreaterThanOrEqal = num10 >= num9;// false
+
+
+        }
 
 }
