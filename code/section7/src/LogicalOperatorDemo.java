151e0a9e3ce71c9107aa00763e677b0d85182bf3
Raj Khare <raj.khare.java@gmail.com>
2024-10-10 21:01:20 +0530

    10-Oct-2024:update

---
+++ code/section7/src/LogicalOperatorDemo.java
@@ -0,0 +1,15 @@
+public class LogicalOperatorDemo {
+
+    public static void main(String[] args) {
+
+        boolean isValid;
+
+        isValid = !true;
+        isValid = !false;
+        int num1 = 9;
+        int num2 = 6;
+        isValid = !(num1>num2);
+
+    }
+
+}
