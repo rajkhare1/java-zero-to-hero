485bb0188f883fdfe23d22c4ce3d795ff5d06ba9
rajkhare1 <raj.khare.java@gmail.com>
2024-10-28 20:36:09 +0530

    28-Oct-2024:update

--- code/section7/src/BitwiseOperatorsDemo.java
+++ code/section7/src/BitwiseOperatorsDemo.java
@@ -20,6 +20,21 @@ public class BitwiseOperatorsDemo {
         int z4 = x1 << 2; // 40 =  10*4
         System.out.println("z4: "+z4);
 
+        int x2 = 50;
+        int x3 = -50;
+        int z5 = x2 >> 3; //6
+        System.out.println(z5);
+        int z6 = x3 >> 3; //-7
+        System.out.println(z6);
+
+        int z7 = x1 >>> 2; //2
+        System.out.println(z7);
+
+        int x4 = 10;
+        int y4 = 6;
+        x4 &= y4;
+        System.out.println(x4); //2
+
     }
 
 }
