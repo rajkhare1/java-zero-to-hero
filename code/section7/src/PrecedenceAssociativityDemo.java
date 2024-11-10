4339dbf14f6af30575ea1df9034aadb4d8ba6b32
rajkhare1 <raj.khare.java@gmail.com>
2024-11-10 16:42:11 +0530

    update:10-11-2024

---
+++ code/section7/src/PrecedenceAssociativityDemo.java
@@ -0,0 +1,11 @@
+public class PrecedenceAssociativityDemo {
+
+    public static void main(String[] args) {
+        int calNum = (16 - 8) * 2;// 16
+        System.out.println(calNum);
+
+        calNum = 16 * (5 /2);//32
+        System.out.println(calNum);
+    }
+
+}
---
+++ code/section7/src/TernaryOperatorDemo.java
@@ -0,0 +1,13 @@
+public class TernaryOperatorDemo {
+
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int max = (x > y) ? x : y;
+        System.out.println(max);
+
+        boolean isEven =  ((14%2) == 0) ? true : false;
+        System.out.println("isEven: "+isEven);
+    }
+
+}
