bf45eff42162e3812e482dedf7e30c21217ab2b1
Raj Khare <raj.khare.java@gmail.com>
2024-11-28 05:22:55 +0530

    updateL0:28-11-2024

---
+++ code/section8/src/DoWhileStatementDemo.java
@@ -0,0 +1,13 @@
+public class DoWhileStatementDemo {
+
+    public static void main(String[] args) {
+
+        int num = 6;
+
+        do {
+            System.out.println(num);
+            num++;
+        } while (num < 6);
+    }
+
+}
