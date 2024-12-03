802f535e3aba85b76de6cb4dad8f2186da256340
Raj Khare <raj.khare.java@gmail.com>
2024-12-03 10:27:53 +0530

    updateL0:03-12-2024

--- code/section8/src/BreakStatementDemo.java
+++ code/section8/src/BreakStatementDemo.java
@@ -10,7 +10,7 @@ public class BreakStatementDemo {
         }
 
         int[] arrayOfInt = {43,455,4376,343,6};
-        int lookingFor = 455;
+        int lookingFor = 4551;
         boolean isFound = false;
         for(int i=0; i<arrayOfInt.length;i++){
             if(lookingFor==arrayOfInt[i]){
@@ -18,6 +18,25 @@ public class BreakStatementDemo {
                 break;
             }
         }
+
+        if(isFound) {
+            System.out.println("Hurrah number is found");
+        } else {
+            System.out.println("Oops! Number is not found");
+        }
+
+        outerForLoop:
+        for(int i = 1; i<5; i++){
+            innerForLoop:
+            for(int j = 1; j<3; j++){
+                System.out.println("i = "+i+" and j = "+j);
+
+                if(i == 3){
+                    break outerForLoop;
+                }
+            }
+        }
+
 
     }
 
