9f351f9a5581c610fbeea41040ae7565015e5ad3
Raj Khare <raj.khare.java@gmail.com>
2025-01-12 21:16:12 +0530

    abstract class and abstract method examples

---
+++ code/section_10/src/com/rajkhare/marvel/base/Subject.java
@@ -0,0 +1,12 @@
+package com.rajkhare.marvel.base;
+
+public abstract class Subject {
+
+    // concrete methods
+    public int totalMarks () {
+        return 100;
+    }
+
+    public abstract void teach ();
+
+}
---
+++ code/section_10/src/com/rajkhare/marvel/subject/Mathematics.java
@@ -0,0 +1,11 @@
+package com.rajkhare.marvel.subject;
+
+import com.rajkhare.marvel.base.Subject;
+
+public class Mathematics extends Subject {
+
+    @Override
+    public void teach() {
+        System.out.println("I am trying to teach Mathematics");
+    }
+}
