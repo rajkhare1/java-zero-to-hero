8633371a478bb88bdda595a60b2b2de052117ac7
Raj Khare <raj.khare.java@gmail.com>
2025-02-20 05:00:37 +0530

    [JAVA10] var

---
+++ code/section_15/src/com/rajkhare/object/demo/VarDemo.java
@@ -0,0 +1,11 @@
+package com.rajkhare.object.demo;
+
+public class VarDemo {
+
+    public static void main(String[] args) {
+        var message = "Hello World !";
+        var person = new Person();
+        var arr = new ArrayIndexOutOfBoundsException();
+    }
+
+}
