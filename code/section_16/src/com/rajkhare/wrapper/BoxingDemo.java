31676f1036b55559a1d1bc891cc7c0081b9dbe0b
raj.khare@oracle.com <raj.khare@oracle.com>
2025-02-20 20:28:03 +0530

    wrapper classes

---
+++ code/section_16/src/com/rajkhare/wrapper/BoxingDemo.java
@@ -0,0 +1,14 @@
+package com.rajkhare.wrapper;
+
+public class BoxingDemo {
+
+    public static void main(String[] args) {
+        int num = 16;
+
+        // Approach 1 - With the help of the Constructor
+        Integer integer = new Integer(num);
+        Double aDouble = new Double(3.14);
+        Long aLong = new Long("95657");
+    }
+
+}
