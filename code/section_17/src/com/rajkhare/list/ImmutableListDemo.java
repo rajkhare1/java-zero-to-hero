73bcad1864d9af513266282efeec6420fbe4d94e
raj.khare@oracle.com <raj.khare@oracle.com>
2025-02-27 19:58:57 +0530

    Immutable list

---
+++ code/section_17/src/com/rajkhare/list/ImmutableListDemo.java
@@ -0,0 +1,16 @@
+package com.rajkhare.list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ImmutableListDemo {
+
+    public static void main(String[] args) {
+        List<Integer> accountNumbers = new ArrayList<>();
+        accountNumbers.add(325454);
+        accountNumbers.add(325455);
+        accountNumbers.add(325456);
+
+    }
+
+}
