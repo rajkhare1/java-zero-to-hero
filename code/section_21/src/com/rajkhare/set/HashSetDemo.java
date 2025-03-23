ac06d5b7d329cc0450070af81dffc349423d662e
Raj Khare <raj.khare.java@gmail.com>
2025-03-24 05:28:54 +0530

    Hash,HashSet

---
+++ code/section_21/src/com/rajkhare/set/HashSetDemo.java
@@ -0,0 +1,12 @@
+package com.rajkhare.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetDemo {
+
+    public static void main(String[] args) {
+        Set<String> visitedCountry =  new HashSet<>();
+    }
+
+}
