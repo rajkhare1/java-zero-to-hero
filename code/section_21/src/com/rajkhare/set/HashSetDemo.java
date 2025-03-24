46ed970fa02fd6a488cb5536e050242de56136af
Raj Khare <raj.khare.java@gmail.com>
2025-03-25 05:21:50 +0530

    HashSet Iteration

--- code/section_21/src/com/rajkhare/set/HashSetDemo.java
+++ code/section_21/src/com/rajkhare/set/HashSetDemo.java
@@ -2,11 +2,27 @@ package com.rajkhare.set;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Logger;
 
 public class HashSetDemo {
 
+    private static Logger logger = Logger.getLogger(HashSetDemo.class.getName());
+
     public static void main(String[] args) {
-        Set<String> visitedCountry =  new HashSet<>();
+        Set<String> visitedCountries =  new HashSet<>();
+        visitedCountries.add("India");
+        visitedCountries.add("USA");
+        visitedCountries.add("Dubai");
+        visitedCountries.add("Singapore");
+        visitedCountries.add("France");
+        visitedCountries.add("India");
+        logger.info("Size; "+visitedCountries.size());// how many elements are stored in my collection.
+        logger.info(""+visitedCountries);
+        boolean isDubai = visitedCountries.contains("Dubai");
+        logger.info("isDubai: "+isDubai);
+        boolean isRemovedSingapore = visitedCountries.remove("Singapore");
+        logger.info("isRemovedSingapore: "+isRemovedSingapore);
+        logger.info(""+visitedCountries);
     }
 
 }
---
+++ code/section_21/src/com/rajkhare/set/HashSetIterationDemo.java
@@ -0,0 +1,25 @@
+package com.rajkhare.set;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashSetIterationDemo {
+
+    public static void main(String[] args) {
+        Set<String> superpowers = new HashSet<>();
+        // Superheros and their unique abilities
+        superpowers.add("Invisibility");
+        superpowers.add("");
+        superpowers.add("");
+        superpowers.add("");
+        superpowers.add("");
+        
+        superpowers.add("");
+        superpowers.add("");
+        superpowers.add("");
+        superpowers.add("");
+        superpowers.add("");
+        superpowers.add("");
+    }
+
+}
