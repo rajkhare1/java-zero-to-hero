86e6009c8e6a317b73f81de60b2dff5fc04da795
raj.khare@oracle.com <raj.khare@oracle.com>
2025-04-14 17:10:00 +0530

    Predicate

---
+++ code/section_24/src/com/rajkhare/lambda/PredicateDemo.java
@@ -0,0 +1,11 @@
+package com.rajkhare.lambda;
+
+import java.util.function.Predicate;
+
+public class PredicateDemo {
+
+    public static void main(String[] args) {
+        Predicate<Integer> isEven
+    }
+
+}
