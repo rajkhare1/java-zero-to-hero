6942faa0a14e0a56f5b466d1d90b449eb77df81f
Raj Khare <raj.khare.java@gmail.com>
2025-04-26 04:36:50 +0530

    refactoring

--- code/section_26/src/com/rajkhare/streams/StreamFlatMapOperationDemo.java
+++ code/section_26/src/com/rajkhare/streams/StreamFlatMapOperationDemo.java
@@ -12,16 +12,23 @@ public class StreamFlatMapOperationDemo {
         Stream<String> streamOfWords = Arrays.stream(arrayOfWords);
         Stream<String[]> streamOfLetters = streamOfWords.map(word -> word.split(""));
 //        streamOfLetters.forEach(System.out::println);// Problem: [Ljava.lang.String;@32e6e9c3
-        streamOfLetters.flatMap(Arrays::stream).forEach(System.out::println);
+        streamOfLetters
+                .flatMap(Arrays::stream)
+                .forEach(System.out::println);
 
-        List<Person> persons = Arrays.asList(new Person("Alice", Arrays.asList("123", "456")), new Person("Bob", Arrays.asList("789", "101", "112")));
+        List<Person> persons = Arrays.asList(new Person("Alice", Arrays.asList("123", "456"))
+                , new Person("Bob", Arrays.asList("789", "101", "112")));
 
-        List<List<String>> mapResult = persons.stream().map(Person::getPhoneNumbers).collect(Collectors.toList());
+        List<List<String>> mapResult = persons.stream()
+                .map(Person::getPhoneNumbers)
+                .collect(Collectors.toList());
 
         System.out.println(mapResult);// [[123, 456], [789, 101, 112]] not in decent format
 
         // using flat map
-        List<String> flatMapResult = persons.stream().flatMap(person -> person.getPhoneNumbers().stream()).collect(Collectors.toList());
+        List<String> flatMapResult = persons.stream()
+                .flatMap(person -> person.getPhoneNumbers().stream())
+                .collect(Collectors.toList());
 
         System.out.println(flatMapResult);// [123, 456, 789, 101, 112]
 
