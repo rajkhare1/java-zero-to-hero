54c7ec41d97c0c1ba7c22b1575eb75b64f0446e1
raj.khare@oracle.com <raj.khare@oracle.com>
2025-05-01 18:25:52 +0530

    FindingAndMatchingOperations[allMatch(), anyMatch(), noneMatch(), findAny(), findFirst()]

---
+++ code/section_26/src/com/rajkhare/streams/StreamFindMatchOperationDemo.java
@@ -0,0 +1,46 @@
+package com.rajkhare.streams;
+
+import java.util.List;
+import java.util.Optional;
+
+public class StreamFindMatchOperationDemo {
+
+    public static void main(String[] args) {
+        List<Item> items = getItems();
+
+        // Check if all items in stock
+        boolean allInStock = items.stream().allMatch(Item::isInStock);
+        System.out.println(allInStock);// false
+
+        // Check if any item is on sale
+        boolean anyOnSale = items.stream().anyMatch(Item::isOnSale);
+        System.out.println(anyOnSale);// ture
+
+        // Check if any item is out of stock
+        boolean anyOutOfStock = items.stream().anyMatch(item -> !item.isInStock());
+        System.out.println(anyOutOfStock);// ture
+
+        // Check if all items less than or equal to 5000
+        boolean isAnyCostlyProduct = items.stream().noneMatch(item -> item.getPrice() > 5000);
+        System.out.println(isAnyCostlyProduct);// true
+
+        // Fetch a random item that has price greater than 1000
+        Optional<Item> randomItemOptional = items.stream().filter(item -> item.getPrice() > 1000).findAny();
+        System.out.println(randomItemOptional);
+
+        // Fetch a first item that has price greater than 1000
+        Optional<Item> firstItemOptional = items.stream().filter(item -> item.getPrice() > 1000).findFirst();
+        System.out.println(firstItemOptional);
+    }
+
+    private static List<Item> getItems() {
+        return List.of(
+                new Item("Laptop", 1200, true, false),
+                new Item("Smartphone", 800, true, true),
+                new Item("Headphone", 150, false, true),
+                new Item("Camera", 2000, true, false),
+                new Item("Smartwatch", 300, true, false)
+        );
+    }
+
+}
