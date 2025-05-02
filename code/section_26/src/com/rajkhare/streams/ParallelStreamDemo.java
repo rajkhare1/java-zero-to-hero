9054605a2210a814d432bff75f1e208666d40c21
raj.khare@oracle.com <raj.khare@oracle.com>
2025-05-02 19:37:20 +0530

    ParallelStream

--- code/section_26/src/com/rajkhare/streams/ParallelStreamDemo.java
+++ code/section_26/src/com/rajkhare/streams/ParallelStreamDemo.java
@@ -6,9 +6,18 @@ import java.util.stream.Stream;
 public class ParallelStreamDemo {
 
     public static void main(String[] args) {
-        Stream<Integer> numStream = Stream.iterate(1, num -> num + 1).limit(100);
-
-        System.out.println(numStream.map(num -> num * 2).collect(Collectors.toList()));
+        Stream<Integer> numStream = Stream.iterate(1, num -> num + 1).limit(100).parallel();
+        long startTime = System.currentTimeMillis();
+        System.out.println(numStream.map(num -> {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            return num * 2;
+        }).collect(Collectors.toList()));
+        long endTime = System.currentTimeMillis();
+        System.out.println(endTime-startTime);
     }
 
 }
