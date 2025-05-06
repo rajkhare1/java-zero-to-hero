f8fd3dfbddfa5d1d46148a04faf278afe583240a
Raj Khare <raj.khare.java@gmail.com>
2025-05-07 05:00:22 +0530

    [new Thread(runnable).start()]

--- code/section_28/src/com/rajkhare/multithreading/ThreadDemo.java
+++ code/section_28/src/com/rajkhare/multithreading/ThreadDemo.java
@@ -8,6 +8,13 @@ public class ThreadDemo {
 
         Thread thread1 = new Thread(() -> System.out.println("Hello from Java Thread"));
         thread1.start();
+
+        Runnable runnable = () -> System.out.println("Hi from Java Thread");
+        Thread thread2 = new Thread(runnable);
+        thread2.start();
+
+        System.out.println("Hello from Main Thread");
+
     }
 
 }
