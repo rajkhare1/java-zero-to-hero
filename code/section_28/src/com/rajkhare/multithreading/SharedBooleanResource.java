d584469b8aeb116e3ce44f3fe2383d60a8ad1401
Raj Khare <raj.khare.java@gmail.com>
2025-05-14 05:09:45 +0530

    [volatile keywork]

---
+++ code/section_28/src/com/rajkhare/multithreading/SharedBooleanResource.java
@@ -0,0 +1,18 @@
+package com.rajkhare.multithreading;
+
+public class SharedBooleanResource {
+
+    private volatile boolean stopFlag = false;
+
+    public void setStopFlag() {
+        stopFlag = true;
+    }
+
+    public void doWork() {
+        while (!stopFlag) {
+            System.out.println("Working...");
+        }
+        System.out.println("Work stopped");
+    }
+
+}
