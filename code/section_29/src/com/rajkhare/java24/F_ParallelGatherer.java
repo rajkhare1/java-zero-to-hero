9aa107639c295ecdbd80f97a33cf3713fb72dcda
Raj Khare <raj.khare.java@gmail.com>
2025-05-28 05:04:13 +0530

    Parallel Gatherer

--- code/section_29/src/com/rajkhare/java24/F_ParallelGatherer.java
+++ code/section_29/src/com/rajkhare/java24/F_ParallelGatherer.java
@@ -2,6 +2,7 @@ package com.rajkhare.java24;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Gatherer;
+import java.util.stream.Stream;
 
 public class F_ParallelGatherer {
 
@@ -9,9 +10,24 @@ public class F_ParallelGatherer {
         Gatherer<Integer, ?, Integer> parallelSum =
                 Gatherer.of(AtomicInteger::new,
                         (sum, number, downstream) -> {
+                    System.out.printf("[Integrator] Thread: %s | number: %d%n", Thread.currentThread().getName(),number);
                             sum.addAndGet(number);
                             return true;
-                        });
+                        },
+                        (s1,s2) -> {
+                            System.out.printf("[Combiner] Thread: %s | combining: %d + %d%n",
+                                    Thread.currentThread().getName(),s1.get(),s2.get());
+                        s1.addAndGet(s2.get());// combiner
+                            return s1;
+                        },
+                        (sum, downstream) -> {
+                            System.out.printf("[Finisher] Thread: %s | result: %d%n", Thread.currentThread().getName(),sum.get());
+                    downstream.push(sum.get());
+                });
+
+        var result = Stream.of(10,20,30,40,50,60,70,80,90,100).parallel().gather(parallelSum).toList();
+        System.out.println(result);
+
     }
 
 }
